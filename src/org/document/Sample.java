f294c9166b9814f528f91c81c486f19c20fd128a
bharathivenkat <bharathit01@gmail.com>
2018-04-26 22:08:36 +0530

    deleting a word

--- src/org/document/Sample.java
+++ src/org/document/Sample.java
@@ -3,7 +3,7 @@ package org.document;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("first document");
+		System.out.println("document");
 		System.out.println("second document");
 
 	}
